1aeb9a6ef6461dc036ebc9d29bb362ef9e415d66
aelgy <aelgy333@gmail.com>
2023-10-31 13:34:36 +0800

    build: use express-handlebars as a view engine

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// 載入 HandleBars
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.redirect('/movies')
 })
 
+// 設定路由
 app.get('/movies', (req, res) => {
-  res.send('listing movies')
+  res.render('index')
 })
 
 app.get('/movie/:id', (req, res) => {
